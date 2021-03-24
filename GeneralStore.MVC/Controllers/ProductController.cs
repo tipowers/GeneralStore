f30ff8bdb8ecf87da2c330a560dec4ac238cee4f
tipowers <tpowers88@outlook.com>
2021-03-24 13:23:19 -0400

    created project scaffolding and added Product model, view, and controller

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add link to DB using DBContext
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+    }
+}
