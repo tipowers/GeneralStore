e94cd3a83bc62291292801796b19bdbb6242016c
tipowers <tpowers88@outlook.com>
2021-03-24 15:27:54 -0400

    added product details mvc functionality

--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -16,7 +16,9 @@ namespace GeneralStore.MVC.Controllers
         // GET: Product
         public ActionResult Index()
         {
-            return View(_db.Products.ToList());
+            List<Product> productList = _db.Products.ToList();
+            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();
+            return View(orderedList);
         }
 
         // Get: Product
@@ -79,7 +81,7 @@ namespace GeneralStore.MVC.Controllers
             return View(product);
         }
 
-        //POST: Product/Edit/{id}
+        // POST: Product/Edit/{id}
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Product product)
@@ -89,6 +91,21 @@ namespace GeneralStore.MVC.Controllers
                 _db.Entry(product).State = System.Data.Entity.EntityState.Modified;
                 _db.SaveChanges();
                 return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
             }
             return View(product);
         }
