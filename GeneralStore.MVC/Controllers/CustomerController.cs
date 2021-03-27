aa85b661d21effe3852009ea6d0768556bfb36f2
tipowers <tpowers88@outlook.com>
2021-03-27 10:36:11 -0400

    added customer mvc and updated database via migration

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer/Index
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+    }
+}
